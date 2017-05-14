f49b54681a8a1a985be3663eaf666859d79758eb
John <ImmortalYang@gmail.com>
2017-05-14 18:26:00 +1200

    Change logo icon, style header and footer and page sections. Complete About page.

--- src/QualityBags/Controllers/HomeController.cs
+++ src/QualityBags/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace QualityBags.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Lorem Ipsum.";
 
             return View();
         }
--- src/QualityBags/Gulp/Tasks/Less.js
+++ src/QualityBags/Gulp/Tasks/Less.js
@@ -1,9 +1,10 @@
 ﻿var gulp = require("gulp");
 var less = require("gulp-less");
+var autoprefixer = require("autoprefixer");
 
 gulp.task("less", function () {
     return gulp.src('./wwwroot/css/styles.less')
-            .pipe(less())
+            .pipe(less([autoprefixer]))
             .on('error', function (errorInfo) {
                 console.log(errorInfo.toString());
                 this.emit('end');
